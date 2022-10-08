b2944522f33116baaff8ccde3d752c1b99914f11
ArgetnarYT <104316740+ArgetnarYT@users.noreply.github.com>
2022-10-08 13:47:15 +0300

    Update Sheep_Tycoon.lua

--- Sheep_Tycoon.lua
+++ Sheep_Tycoon.lua
@@ -1,82 +1,39 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "KeyHub Argetnar", HidePremium = true, SaveConfig = true, ConfigFolder = "OrionTest"})
-
-OrionLib:MakeNotification({
-	Name = "Your in!",
-	Content = "Argetnar KeyHub v128", 
-	Image = "rbxassetid://4483345998",
-	Time = 5
-})
+local ArgetnarLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/ArgetnarYT/Library/main/ArgetnarLibrary.lua"))()
+local win = ArgetnarLib:Window("Argetnar Hub")
+ArgetnarLib:Notify("KeyHub", "Yuor in!")
+local TabKey = win:Tab("Key")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ArgetnarYT/scripts/main/Key.lua"))()
 
+
 function MakeScriptHub()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ArgetnarYT/scripts/main/Sheep%20Tycoon.lua"))()
-
-
 end
 
-
 function CorrectKeyNotifications()
-    OrionLib:MakeNotification({
-        Name = "Correct Key",
-        Content = "Welcome :D",
-        Image = "rbxassetid://4483345998",
-        Time = 2
-    })
+ArgetnarLib:Notify("KeyHub", "Key Correct!")
 end
 
 function WrongKeyNotifications()
-    OrionLib:MakeNotification({
-        Name = " Wrong Key",
-        Content = "Wrong key",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
+ArgetnarLib:Notify("KeyHub", "Key Wrong!")
 end
 
 function GetKeyNotifications()
-    OrionLib:MakeNotification({
-        Name = "GetKey",
-        Content = "Discord Link Copied =)",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
+ArgetnarLib:Notify("KeyHub", "Discord Link Copied =)")
 end
-
-local Tab = Window:MakeTab({
-	Name = "Key",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-Tab:AddTextbox({
-	Name = "Enter Key",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-        _G.KeyInput = Value
-	end	  
-})
-
-Tab:AddButton({
-	Name = "Check Key!",
-	Callback = function()
-        if _G.KeyInput == _G.EnterKey then
+TabKey:TextBox("Enter Key", function(Value)
+       _G.KeyInput = Value
+end)
+TabKey:Button("Check Key!", function()
+            if _G.KeyInput == _G.EnterKey then
 			CorrectKeyNotifications()
 			wait(3)
          MakeScriptHub()
      else
         WrongKeyNotifications()
         end
-  	end    
-})
-
-Tab:AddLabel("Get Key")
-Tab:AddButton({
-	Name = "Copy link!",
-	Callback = function()
-      		setclipboard("https://discord.gg/DaKyeHFx8d")
+end)
+TabKey:Button("GetKey", function()
+          		setclipboard("https://discord.gg/w7H6G66xCK")
 			  GetKeyNotifications()
-  	end    
-})
+end)
