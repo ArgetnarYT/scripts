c470d8e9ae2a27e6ad2101d3dad7e32d6f35e747
ArgetnarYT <104316740+ArgetnarYT@users.noreply.github.com>
2022-10-08 13:16:18 +0300

    Update Sheep Tycoon.lua

--- Sheep Tycoon.lua
+++ Sheep Tycoon.lua
@@ -1,247 +1,76 @@
---library
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
-local Window = OrionLib:MakeWindow({Name = "Sheep Tycoon 🐑", HidePremium = true, SaveConfig = true, ConfigFolder = "Argetnar Hack"})
-local Tab = Window:MakeTab({
-	Name = "Main",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local Section = Tab:AddSection({
-	Name = "Universal-Script"
-})
+local ArgetnarLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/ArgetnarYT/Library/main/ArgetnarLibrary.lua"))()
+local win = ArgetnarLib:Window("Argetnar Hub")
+ArgetnarLib:Notify("Script", "Loading....")
+local TabFarm = win:Tab("Autofarm")
 
-Tab:AddButton({
-	Name = "Reset",
-	Callback = function()
-      		game.Players.LocalPlayer.Character.Humanoid.Health = 0
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Kick",
-	Callback = function()
-     game.Players.LocalPlayer:Kick("bye")
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Rejoin",
-	Callback = function()
-     local ts = game:GetService("TeleportService")
-
-     local p = game:GetService("Players").LocalPlayer
-
-
-
-     ts:TeleportToPlaceInstance(game.PlaceId, game.JobId, p)
-  	end    
-})
-
-Tab:AddSlider({
-	Name = "Walkspeed",
-	Min = 0,
-	Max = 200,
-	Default = 5,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "Speed",
-	Callback = function(s)
-		 game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-	end    
-})
-
---ESP
-local Tab = Window:MakeTab({
-        Name = "ESP",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-})
-
-getgenv().cham = false
-getgenv().nameESP = false
-getgenv().boxESP = false
-
-
-getgenv().esp_loaded = false
-getgenv().Visibility = false
-
-
-Tab:AddToggle({
-    Name = "Visual",
-    Default = getgenv().Visibility,
-    Callback = function(Value)
-        if getgenv().esp_loaded == false and Value == true then
-            getgenv().esp_loaded = true
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/skatbr/Roblox-Releases/main/A_simple_esp.lua", true))()
-        end
-        getgenv().Visibility = Value
-    end   
-})
-
-Tab:AddToggle({
-    Name = "Box ESP",
-    Default = getgenv().Visibility,
-    Callback = function(Value)
-        if getgenv().esp_loaded == false and Value == true then
-            getgenv().esp_loaded = true
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/skatbr/Roblox-Releases/main/A_simple_esp.lua", true))()
-        end
-        getgenv().boxESP = Value
-    end   
-})
-
-
-Tab:AddToggle({
-    Name = "Name",
-    Default = getgenv().Visibility,
-    Callback = function(Value)
-        if getgenv().esp_loaded == false and Value == true then
-            getgenv().esp_loaded = true
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/skatbr/Roblox-Releases/main/A_simple_esp.lua", true))()
-        end
-        getgenv().nameESP = Value
-    end   
-})
-
-
-Tab:AddToggle({
-    Name = "Chams",
-    Default = getgenv().Visibility,
-    Callback = function(Value)
-        if getgenv().esp_loaded == false and Value == true then
-            getgenv().esp_loaded = true
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/skatbr/Roblox-Releases/main/A_simple_esp.lua", true))()
-        end
-        getgenv().cham = Value
-    end   
-})
-
-
-function SendNote(message : string, time)
-    OrionLib:MakeNotification({
-        Name = "Title!",
-        Content = message,
-        Image = "rbxassetid://4483345998",
-        Time = time or 3
-    })
-end
-
-
-
-Tab:AddToggle({
-        Name = "Use Team-Color",
-        Default = false,
-        Callback = function(Value)
-            getgenv().useTeamColor = Value
-        end
-})
-local Tab = Window:MakeTab({
-	Name = "Sheep Tycoon 🐑",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "Auto Sell Wools",
-	Callback = function()
-      		while wait() do
-   game:GetService("ReplicatedStorage").Signals.RemoteEvents.PutRemote:FireServer()
-end
-  	end    
-})
-
-Tab:AddToggle({
-    Name = "Auto Obby",
-    Default = false,
-    Callback = function(AutoObby)
-
-    OrionLib:MakeNotification({
-	Name = "Script",
-	Content = "this script was made 1234567#3209",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-}) 
-setclipboard("1234567#3209")
-local Player = game:GetService("Players").LocalPlayer.Name
+TabFarm:Toggle("AutoFarm Wool", function(AutoFarm)
+            _G.autowool = AutoFarm
+while _G.autowool == true do
+    game:GetService("ReplicatedStorage").Signals.RemoteEvents.GetWoolRemote:FireServer(17)
+    wait()
+    end
+end)
+TabFarm:Toggle("Auto Obby", function(AutoObby)
+    local Player = game:GetService("Players").LocalPlayer.Name
         _G.doublemoney = AutoObby
         while _G.doublemoney == true do
         game:GetService("Workspace")[(Player)].HumanoidRootPart.CFrame = CFrame.new(-935.611084, 186.181, 837.546143, 0, 0, 1, 0, 1, -0, -1, 0, 0)
         wait()
         end
-    end   
-})
-
-Tab:AddToggle({
-    Name = "Auto Wool",
-    Default = false,
-    Callback = function(AutoFarm)
-        _G.autowool = AutoFarm
-while _G.autowool == true do
-    game:GetService("ReplicatedStorage").Signals.RemoteEvents.GetWoolRemote:FireServer(17)
-    wait()
-    end
+end)
+TabFarm:Button("AutoSell Wool", function()
+          		while wait() do
+   game:GetService("ReplicatedStorage").Signals.RemoteEvents.PutRemote:FireServer()
 end
-})
+end)
+TabFarm:Button("Auto Code", function()
+local A_1 = "SheepTycoon"
+local Event = game:GetService("Players").MinyWonk.PlayerGui.ScreenGui.Code.CodeRemote
+Event:FireServer(A_1)
+end)
 
-Tab:AddButton({
-	Name = "Infinity Money[Test Version]",
-	Callback = function()
-      		local args = {
-    [1] = 17,
-    [2] = Vector3.new(-162.36593627929688, 3.311232566833496, 938.9034423828125)
-}
+local TabMisc = win:Tab("Misc")
 
-game:GetService("ReplicatedStorage").Signals.RemoteEvents.GetWoolRemote:FireServer(unpack(args))
-  	end    
-})
+TabMisc:TextBox("Chat Writer", function(Value)
+local A_1 = Value
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+end)
+TabMisc:Slider("Walk Speed", 16, 500, 50, function(Value)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+end)
+
+TabMisc:Button("Destroy GUI", function()
+    if game.CoreGui:FindFirstChild("MangoHub") then
+    game.CoreGui.MangoHub:Destroy();
+end
+end)
+TabMisc:Button("Rejoin", function()
+local ts = game:GetService("TeleportService")
+
+local p = game:GetService("Players").LocalPlayer
+
+ts:TeleportToPlaceInstance(game.PlaceId, game.JobId, p)
+end)
+
+local TabTp = win:Tab("Teleports")
+TabTp:Button("Tp End 1 Obby", function()
+     local Player = game:GetService("Players").LocalPlayer.Name
+	
+game:GetService("Workspace")[(Player)].HumanoidRootPart.CFrame = CFrame.new(-910.353577, 181.059067, 837.684326, 0.0206528772, 5.01800947e-08, 0.999786735, -2.26483987e-08, 1, -4.97229458e-08, -0.999786735, -2.16166463e-08, 0.0206528772)
+
+end)
+TabTp:Button("Tp End 2 Obby", function()
+     local Player = game:GetService("Players").LocalPlayer.Name
+	
+game:GetService("Workspace")[(Player)].HumanoidRootPart.CFrame = CFrame.new(619.623352, 123.516922, 754.34259, 0.961078823, 1.44117553e-08, 0.276274294, -1.95509813e-08, 1, 1.5847581e-08, -0.276274294, -2.06322071e-08, 0.961078823)
+
+end)
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-local Credits = Window:MakeTab({
-	Name = "Credits",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Credits:AddParagraph("Made by:","Brinen & Argetnar & Doku & 1234567#3209.")
-wait(0.5)
-Credits:AddParagraph("Script on Sheep Tycoon 🐑","")
-
-Credits:AddButton({
-	Name = "Discord Server copy",
-	Callback = function()
-      		setclipboard("https://discord.gg/DaKyeHFx8d")
-			  OrionLib:MakeNotification({
-	Name = "Script",
-	Content = "Discord Link has been copy!",
-	Image = "rbxassetid://4483345998",
-	Time = 2
-})
-
-  	end    
-})
+ArgetnarLib:Notify("Script", "Was Loaded!")
